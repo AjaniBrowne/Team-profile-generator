5e14bd746c9fd2c9612795571924db582c8d2bf3
AjaniBrowne <ajani.browne@yahoo.com>
2022-07-24 21:17:04 -0400

    commit on main " added boostrap and for loop for employees"

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,12 +1,32 @@
 const generateManager = (manager) => {
-    return `<div><h3>${manager.getName()}</h3><h4>${manager.getofficeNumber()}</h4></div>`
+    return `<div class="col-5 bg-light rounded text-center">
+    <h3>${manager.getName()}</h3>
+    <h4>${manager.getId()}</h4>
+    <p><b>Email:</b> ${manager.getEmail()}</p>
+    <p><b>Spec:</b> ${manager.getofficeNumber()}</p>
+</div>`
 }
 const generateIntern = (intern) => {
-    return `<div><h3>${intern.getName()}</h3><h4>${intern.getSchool()}</h4></div>`
+    return `<div class="col-5 bg-light rounded text-center">
+    <h3>${intern.getName()}</h3>
+    <h4>${intern.getId()}</h4>
+    <p><b>Email:</b> ${intern.getEmail()}</p>
+    <p><b>Spec:</b> ${intern.getSchool()}</p>
+</div>`
+}
+
+const generateEngineer = (engineer) => {
+    return ` <div class = " col-5 bg-light rounded text-center">
+    <h3>${engineer.getName()}</h3>
+    <h4>${engineer.getId()}</h4>
+    <p><b>Email:<b>${engineer.getEmail()}</p>
+    <p><b>Spec:<b>${engineer.getGithub()}</p>
+</div> `
+    
 }
 
 const workforce = (team) => {
-    let Manager = [],  Intern = [], Engineer = "";
+    let Manager = [],  Intern = [], Engineer = [];
 
     let allManagers = team.filter(employee => employee.getRole() == 'Manager');
 
@@ -14,6 +34,17 @@ const workforce = (team) => {
         Manager.push(generateManager(allManagers[i]));
     }
 
+    let allInterns = team.filter(employee => employee.getRole() == 'Intern');
+
+    for(let i = 0; i < allInterns.length; i++){
+        Intern.push(generateIntern(allInterns[i]));
+    }
+
+    let allEngineers = team.filter(employee => employee.getRole() == 'Engineer');
+
+    for(let i = 0; i < allEngineers.length; i++){
+        Engineer.push(generateEngineer(allEngineers[i]));
+    }
     return `
     <!DOCTYPE html>
 <html lang="en">
@@ -21,20 +52,21 @@ const workforce = (team) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./dist/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <title>Team Profile Generator</title>
 </head>
-<body>
-    <nav class="team" id="heading">
+<body class ="bg-dark" >
+    <nav class="team bg-primary text-center" id="heading">
         <h1>My Team</h1>
     </nav>
 
-    <div id="Cards">
-        <span>${Manager.join("")}</span>
-        <span>${Intern}</span>
-        <span>${Engineer}</span>
+    <div id="Cards" class="row justify-content-between" >
+        ${Manager.join("")}
+        ${Intern.join("")}
+        ${Engineer.join("")}
     </div>
-    
+    <script src="./generateHtml.js"></script>
 </body>
 </html>`
 };
